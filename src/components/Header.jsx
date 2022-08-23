cba6d42600870e25d435b499384bc15f5de9e5f6
Nay Htet <nayhtet117711@gmail.com>
2022-08-23 21:20:58 +0630

    added home page's body

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,34 @@
 import HomeHeaderBanner from '../assets/home-banner-bg1.svg'
 import HeaderImg from '../assets/header-img1.svg'
+import RightArrowIcon from '../assets/right-arrow.svg'
 
 function Header() {
   return (
     <div className="relative w-full">
       <img alt="header-banner" src={HomeHeaderBanner} className="w-full" />
       <section className='absolute top-0 left-0 right-0 bottom-0 flex-col'>
-        <header className='absolute z-[1] top-0 left-0 right-0 pl-[6rem] pr-[6rem] flex bg-transparent backdrop-blur-lg py-6 px-14'>
+        <header className='sticky z-[1] top-0 left-0 right-0 pl-2 pr-2 md:pl-[6rem] md:pr-[6rem] flex bg-[#00000011] backdrop-blur-lg py-6 px-14'>
           <div className='flex grow'>
             <a href='/' className='text-white font-semibold text-2xl'>UT-YCC</a>
           </div>
           <div className='flex gap-6'>
-            <div className='text-xl text-white cursor-pointer underline-offset-4 duration-100 ease-in-out origin-center hover:scale-[1.01] hover:underline'>Home</div>
-            <div className='text-xl text-white cursor-pointer underline-offset-4 duration-100 ease-in-out origin-center hover:scale-[1.01] hover:underline'>Survey</div>
-            <div className='text-xl text-white cursor-pointer underline-offset-4 duration-100 ease-in-out origin-center hover:scale-[1.01] hover:underline'>Results</div>
+            <div className='text-md md:text-xl text-white cursor-pointer underline-offset-4 duration-100 ease-in-out origin-center hover:scale-[1.01] hover:underline'>Home</div>
+            <div className='text-md md:text-xl text-white cursor-pointer underline-offset-4 duration-100 ease-in-out origin-center hover:scale-[1.01] hover:underline'>Survey</div>
+            <div className='text-md md:text-xl text-white cursor-pointer underline-offset-4 duration-100 ease-in-out origin-center hover:scale-[1.01] hover:underline'>Results</div>
           </div>
         </header>
-        <section className='absolute z-[1] left-[4rem] bottom-[10rem] lg:bottom-[15rem] xl:bottom-[22rem] 2xl:bottom-[24rem] px-14 text-center text-white'>
+        <section className='hidden md:block absolute z-[1] left-[4rem] bottom-[10rem] lg:bottom-[15rem] xl:bottom-[22rem] 2xl:bottom-[24rem] px-14 text-center text-white'>
           <div className='text-2xl lg:text-3xl xl:text-5xl tracking-wide leading-relaxed font-light'>
             University of Technology <br/>
             (Yatanarpon Cyber City)
           </div>
           <div className='mt-6 text-lg lg:text-xl xl:text-2xl tracking-wide font-extralight text-white'>“Shape and bright your future at UTYCC”</div>
         </section>
-        <section className='absolute z-[0] right-[10%] lg:right-[11%] xl:right-[12%] bottom-[4rem] lg:bottom-[6rem] xl:bottom-[8rem]'>
+        <section className='hidden md:block absolute z-[0] right-[10%] lg:right-[11%] xl:right-[12%] bottom-[4rem] lg:bottom-[6rem] xl:bottom-[8rem]'>
           <img alt="header-img" src={HeaderImg} className="w-[18rem] lg:w-[23rem] xl:w-[32rem]" />
         </section>
         <section className='absolute left-[10%] lg:left-[10%] xl:left-[14%] 2xl:left-[18%] bottom-[0rem] lg:bottom-[2rem] xl:bottom-[3rem] 2xl:bottom-[5rem]'>
-          <button className='w-[16rem] text-white py-3 ring-0 rounded-xl bg-gradient-to-r from-[#964DEF] to-[#6C48FC] duration-150 shadow-sm hover:shadow-lg active:ring-2'>Take Survey</button>
+          <button className='flex justify-center items-center gap-3 w-[16rem] text-white py-3 ring-1 ring-white rounded-xl bg-gradient-to-r from-[#964DEF] to-[#6C48FC] duration-150 shadow hover:shadow-lg active:ring-[#6C48FC] active:ring-2'>Take Survey <img src={RightArrowIcon} alt="right-arrow"></img></button>
         </section>
       </section>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,54 @@
+import Img1 from '../assets/img1.svg'
+import Img2 from '../assets/img2.svg'
+import Img3 from '../assets/img3.svg'
+import Img4 from '../assets/img4.svg'
+import RightArrowIcon from '../assets/right-arrow.svg'
+
 function Home() {
   return (
-    <div className="min-h-[100px] py-5">
-      Home
+    <div className="p-5">
+      <ItemRow
+        title="What is this survey?"
+        paragraph="This is a survey for UTYCC graduates. Every year there are many students graduating from UTYCC but there is no systematic information about them yet."
+        imageSrc={Img1}
+      />
+      <ItemRow
+        title=""
+        paragraph="Whether the students working now or is in another academic program, is not known and it would be great to know if the academic plan and years in UTYCC were great experiences for graduated students."
+        imageSrc={Img2}
+        reversed
+      />
+      <ItemRow
+        title="Why you should be taking this survey?"
+        paragraph="By taking this survey, the university can be in check with the trendings in working invironments, can be ahead with updated academic plans and courses, and can know which parts have to be improved."
+        imageSrc={Img3}
+      />
+      <ItemRow
+        title=""
+        paragraph="Juniors can know which kind of working environments their seniors are working on, and can prepare ahead of time for career choices."
+        imageSrc={Img4}
+        reversed
+      />
+      <div className='italic p-5 text-center text-xl font-light'>
+        And lastly, don’t worry. Details of personal data will never be distributed or will not be used for any other purposes.
+      </div>
+      <div className='flex justify-center p-5'>
+        <button className='flex justify-center items-center gap-3 w-[16rem] text-white py-3 ring-1 ring-white rounded-xl bg-gradient-to-r from-[#964DEF] to-[#6C48FC] duration-150 shadow hover:shadow-lg active:ring-[#6C48FC] active:ring-2'>Take Survey <img src={RightArrowIcon} alt="right-arrow"></img></button>
+      </div>
+    </div>
+  )
+}
+
+const ItemRow = ({ title, paragraph, imageSrc, reversed=false }) => {
+  return (
+    <div className={`flex flex-wrap justify-center items-center gap-[1rem] pt-5 ${reversed ? 'flex-row-reverse' : 'flex-row'}`}>
+      <div className="flex flex-col w-full md:max-w-[30em] gap-10">
+        {title && <div className='text-4xl leading-10'>{title}</div>}
+        <div className='text-xl font-light leading-8'>{paragraph}</div>
+      </div>
+      <div className="flex p-5">
+        <img src={imageSrc} className="object-contain w-full md:min-w-[25rem]" />
+      </div>
     </div>
   )
 }
